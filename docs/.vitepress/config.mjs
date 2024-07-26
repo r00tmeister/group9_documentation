ee611e96180e8a9fae64ceeb1fc236c6797928f5
r00tmeister <117168764+r00tmeister@users.noreply.github.com>
2024-07-26 18:36:01 +0200

    reconfigure base to deploy to

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   title: "Documentation Title",
   description: "Documentation Description", 
   appearance: "dark",
+  base: "/group9_documentation/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
